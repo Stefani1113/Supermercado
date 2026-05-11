a7bb847705d8fc3665e37e0544387f8e715ec12b
Pedrito190uwu <mariagaribello190@gmail.com~>
2026-05-11 16:46:49 -0500

    feat: carpetas de seguridad

---
+++ src/main/java/com/proyecto/supermercado/repository/EntradaAlmacenRepository.java
@@ -0,0 +1,7 @@
+package com.proyecto.supermercado.repository;
+
+import com.proyecto.supermercado.entity.EntradaAlmacen;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntradaAlmacenRepository extends JpaRepository<EntradaAlmacen, Long> {
+}
