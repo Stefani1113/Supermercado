ef45a4a72ead71670705ae87fbbf37cd15667dfd
Pedrito190uwu <mariagaribello190@gmail.com~>
2026-05-12 06:54:55 -0500

    fix:solucion de desorde de carpetas

--- src/main/java/com/proyecto/supermercado/repository/EntradaAlmacenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.supermercado.repository;
-
-import com.proyecto.supermercado.entity.EntradaAlmacen;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EntradaAlmacenRepository extends JpaRepository<EntradaAlmacen, Long> {
-}
